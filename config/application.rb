0aa313c8863ce3b485396dfd7c646e4665c3ec6a
Clinton Judy <clinton@j-udy.com>
2013-03-05 17:02:40 -0500

    Forgot to remove unused restful metrics settings.

--- config/application.rb
+++ config/application.rb
@@ -61,9 +61,5 @@ module ArtfullyApp
     end
     
     Delayed::Worker.delay_jobs = false
-    
-    RestfulMetrics::Client.async = true
-    RestfulMetrics::Client.disabled = true if ENV["RESTFUL_METRICS_APP"].blank?
-    RestfulMetrics::Client.set_credentials ENV["RESTFUL_METRICS_API_KEY"]
   end
 end
